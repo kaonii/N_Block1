e2fd58ad9fda40ee688faa634275cbaae40dab88
kaoni <contact@caroline-chevillotte.com>
2021-03-04 11:28:39 +0100

    HelloCampus

--- src/HelloCampus.java
+++ src/HelloCampus.java
@@ -1,5 +1,9 @@
 public class HelloCampus {
     public static void main(String[] args) {
         System.out.print("Hello Campus");
+        System.out.print("Hello");
+        System.out.print("Campus");
+        System.out.print("without");
+        System.out.print("linebreak");
     }
 }
